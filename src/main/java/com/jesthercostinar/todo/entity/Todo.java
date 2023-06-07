c7bb19ab76f36e6c9f5be5289bed50ebc487246f
Jesther Costinar <jesther.jc15@gmail.com>
2023-06-07 20:17:47 +0800

    Create todo entity, hibernate automatically create the entity to database table

---
+++ src/main/java/com/jesthercostinar/todo/entity/Todo.java
@@ -0,0 +1,27 @@
+package com.jesthercostinar.todo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "todos") // @Table annotation makes "tados" table map in this Jpa Entity
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Generate the primary key as AUTO INCREMENT
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+    
+    @Column(nullable = false)
+    private String description;
+
+    private boolean isCompleted;
+}
